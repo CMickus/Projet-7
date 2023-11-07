374f2d74e02c1e2a0dcf03022fe31babfb5033d5
CMickus <mickuscolin@gmail.com>
2023-11-07 08:10:20 +0100

    ajout final avant debug et test

--- routes/Book.js
+++ routes/Book.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const BookCtrl = require('../controllers/Book');
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
+const sharp = require('../middleware/sharp')
 
 router.get('/', BookCtrl.getAllBooks);
-router.post('/', auth, multer, BookCtrl.createBook);
-router.post('/:id/rating', auth, BookCtrl.rateBook)
-router.get('/:id', BookCtrl.getOneBook);
+router.post('/', auth, multer, sharp, BookCtrl.createBook);
+router.post('/:id/rating', auth, BookCtrl.rateBook);
 router.get('/bestrating', BookCtrl.getBestBooks);
-router.put('/:id', auth, multer, BookCtrl.modifyBook);
+router.get('/:id', BookCtrl.getOneBook);
+router.put('/:id', auth, multer, sharp, BookCtrl.modifyBook);
 router.delete('/:id', auth, BookCtrl.deleteBook);
 
 
